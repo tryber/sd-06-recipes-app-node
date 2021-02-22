0eef422652444211403eefef0b1a97af1aa76339
theCoyote <flaviolunaferreira@gmail.com>
2021-02-22 18:28:24 -0300

    requisito 2

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+const { ObjectId } = require('mongodb');
 
 const connection = require('./models/connection');
 
 app.use(bodyParser.json());
 
 app.get('/recipes', async (req, res) => {
-  const resposta = await connection().then((mealDB) => mealDB.collection('recipes').find().toArray())
-  res.status(200).json(resposta)
+    const resposta = await connection()
+      .then((mealDB) => mealDB.collection('recipes').find().toArray())
+    res.status(200).json(resposta)   
+})
+
+app.get('/recipes/:id', async (req, res) => {
+  const { id } = req.params;
+  const resposta = await connection()
+    .then((mealDb) => mealDb.collection('recipes').findOne(ObjectId(id)));
+  if (!id) return res.status(404).json({ message: 'Receita não encontrada' })
+  res.status(200).json(resposta);
 })
 
 app.listen(port, () => console.log(`aplicação rodando na porta ${port}!`))
+
