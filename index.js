5d49d1b5b523f9da33158d19233de02e6fb326f7
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2021-02-22 17:26:30 -0300

    add index.js, model/connection.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log('App listening on PORT %s', PORT)); 
