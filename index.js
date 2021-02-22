f43a23413fda4ab101c97a7754d81537193137af
isabellavjs <isabellavjs@gmail.com>
2021-02-22 18:41:13 -0300

    Finalizando

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3333;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on ${port}!`))
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const RecipesController = require('./controller/RecipesController');
+
+app.use('/', RecipesController);
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message })
+})
+
+app.listen(port, () => console.log(`Example app listening on ${port}!`));
