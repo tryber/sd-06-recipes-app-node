0a6714295f8558800f74a65a21b7b5b67d391409
HCaetano <hrcgeo@gmail.com>
2021-02-22 18:49:00 -0300

    Implementa o CRUD de recipe

--- src/controller/RecipeController.js
+++ src/controller/RecipeController.js
@@ -9,8 +9,35 @@ RecipeController.get('/', async (req, res) => {
 
 RecipeController.get('/:id', async (req, res) => {
   const { id } = req.params;
+  const recipe = await RecipeService.findById(id)
 
-  res.status(200).json(await RecipeService.findById(id));
+  if (!recipe) res.status(404).json({ message: 'Receita não encontrada' });
+
+  res.status(200).json(recipe);
+});
+
+RecipeController.post('/', async (req, res) => {
+  const recipe = req.body;
+  await RecipeService.createRecipe(recipe);
+
+  res.status(201).end();
+});
+
+RecipeController.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const recipe = req.body;
+
+  await RecipeService.updateRecipe(id, recipe);
+
+  res.status(204).end();
+});
+
+RecipeController.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  await RecipeService.deleteRecipe(id);
+
+  res.status(200).json({ message: 'Receita deletada com sucesso' });
 });
 
 module.exports = RecipeController;
--- src/model/Recipe.js
+++ src/model/Recipe.js
@@ -6,10 +6,42 @@ const getAll = async () => {
 };
 
 const findById = async (id) => {
-  return await connection().then((db) => db.collection('recipes').findOne(ObjectId(id)));
+  return await connection().then((db) => db.collection('recipes').findOne(
+    {
+      _id: ObjectId(id)
+    },
+    {
+      fields: {
+        _id: 1,
+        strMeal: 1,
+        strCategory: 1,
+        strInstructions: 1,
+        strTags: 1,
+      }
+    }
+  ));
+};
+
+const createRecipe = async (recipe) => {
+  const { strMeal, strCategory, strArea, strInstructions, strTags } = recipe;
+
+  return await connection().then((db) => db.collection('recipes').insertOne({ strMeal, strCategory, strArea, strInstructions, strTags }));
+};
+
+const updateRecipe = async (id, recipe) => {
+  const { strMeal, strCategory, strArea, strInstructions, strTags } = recipe;
+
+  return await connection().then((db) => db.collection('recipes').updateOne({ '_id': ObjectId(id) }, { $set: { strMeal, strCategory, strArea, strInstructions, strTags } }));
+};
+
+const deleteRecipe = async (id) => {
+  return await connection().then((db) => db.collection('recipes').deleteOne({ _id: ObjectId(id) }));
 };
 
 module.exports = {
   getAll,
   findById,
+  createRecipe,
+  updateRecipe,
+  deleteRecipe,
 };
